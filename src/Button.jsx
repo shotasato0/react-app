876b9dc5dd0ac5a7ba536bf7dd6820f0f2b54798
佐藤翔太 <shota19830706@gmail.com>
2025-05-24 17:08:57 +0900

    [update]Buttonコンポーネントにクリックイベント処理を追加
    ユーザーの操作に反応するようにするため、クリック時にログとアラートを表示する処理を追加しました。

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,6 +1,12 @@
 function Button() {
+
+  const handleClick = () => {
+    console.log("clicked");
+    alert("clicked");
+  };
+
   return (
-    <button type="button">
+    <button type="button" onClick={handleClick}>
       <i>icon</i>
       <span>Click me</span>
     </button>
