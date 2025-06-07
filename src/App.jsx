fc0e98fd6e682861592b6905afb8c38ffa53a95c
佐藤翔太 <shota19830706@gmail.com>
2025-06-07 11:13:33 +0900

    [remove] デモ用UIと関連ロジックを一時削除
    不要になった一時的なUI表示やイベント処理を削除し、最小構成に整理しました。再設計のための準備作業です。

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Button from "./components/Button.jsx";
-import Display from "./components/Display/Display.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <>
-      <h1>Hello World</h1>
-      <Button type="button" disabled={false} onClick={handleClick}>
-        ボタン
-      </Button>
-      <Display count={count} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
