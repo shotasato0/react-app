b03592e76537662c4562bc8d6685f55ebfad2b82
佐藤翔太 <shota19830706@gmail.com>
2025-06-16 17:48:48 +0900

    [add]Homeページへのルーティングを追加
    トップページとしてHomeコンポーネントを表示するようにルーティングを設定しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
