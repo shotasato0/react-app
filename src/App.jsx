249193fc63d120dc9ca7cb023d01ea475ad51c24
佐藤翔太 <shota19830706@gmail.com>
2025-06-16 17:52:05 +0900

    [add]SamplePageへのルーティングを追加
    /sample-pageにアクセスした際にSamplePageコンポーネントが表示されるようにルートを追加しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import SamplePage from "./pages/SamplePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sample-page" element={<SamplePage />} />
       </Routes>
     </BrowserRouter>
   );
